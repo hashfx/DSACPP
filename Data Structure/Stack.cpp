a53d5da801db0d6c40d385c4bb75ff811f152ec3
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-08-30 11:55:29 +0000

    Stack pop

--- Data Structure/Stack.cpp
+++ Data Structure/Stack.cpp
@@ -11,7 +11,7 @@ private:
 public:
     Stack() // constructor
     {
-        top = -1; // stack is empty
+        top = -1; // points to top of the Stack :: -1:stack is empty
         for (int i = 0; i < sizeof(arr); i++)
         {
             arr[i] = 0; // set all values to zero
@@ -37,22 +37,38 @@ public:
     }
 
     // insert element into stack; if space is !available, stack is in StackOverFlow state
-    void push(int value) {
+    void push(int value)
+    {
         // check whether Stack is full or not
         if (isFull())
         {
             cout << "Stack OverFlow" << endl;
-        } else {
-            // push value
-            top++;  // increase top pointer to store value at next block
-            arr[top] = value;  // set value to arr[top] index
         }
-        
+        else
+        {
+            // push value
+            top++;            // increase top pointer to store value at next block
+            arr[top] = value; // set value to arr[top] index
+        }
     }
 
     // return itm at top of stack and removes it; if pop an empty stack, stack is in StackUnderFlow state
-    int pop() {
-
+    int pop()
+    {
+        // check whether Stacj is empty or not
+        if (isEmpty())
+        {
+            cout << "Stack UnderFlow" << endl;
+        }
+        else
+        {
+            // display value of top of the Stack
+            int popped = arr[top]; // store current value in var popped
+            arr[top] = 0;          // set value of arr[top] to 0
+            top--;                 // decrease top to point at lower block value
+            return popped;         // return popped value
+        }
+        return 0;
     }
 
     // access the element at (i)th position
@@ -65,14 +81,23 @@ public:
     void change() {}
 
     // display al elements in stack
-    void display() {}
+    void display()
+    {
+        // iterate Stack to render all values
+        for (int i = top; i >= 0; i--)
+        {
+            cout << arr[i] << endl;
+        }
+    }
 };
 
 int main()
 {
-    Stack s;  // object of Stack
+    Stack s; // object of Stack
     s.push(5);
     s.push(10);
+    s.pop(); // 1644
+    s.display();
 
     return 0;
 }
