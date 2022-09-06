168597b9019a2235a6deea02e55b7f77572a291c
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-09-06 16:33:43 +0000

    Queue isEmpty isFull

--- Data Structure/Queue/Queue.cpp
+++ Data Structure/Queue/Queue.cpp
@@ -3,11 +3,24 @@ using namespace std;
 #define MAX 1000
 class Queue
 {
-    private:
-    int front;
-    int rear;
+private:
+    int front; // removed from front end
+    int rear;  // added from rear end
     int arr[MAX];
+
 public:
+    // constructor
+    Queue()
+    {
+        // initialize front and rear to -1
+        front = -1;
+        rear = -1;
+        for (int i = 0; i < MAX; i++)
+        {
+            arr[i] = 0; // set values of array elements to 0
+        }
+    }
+
     // elements are added from rear end
     void enqueue() {}
 
@@ -15,10 +28,22 @@ public:
     void dequeue() {}
 
     // check if queue is full
-    void isFull() {}
+    bool isFull()
+    {
+        if (rear == sizeof(arr) - 1) // being pointer starting from -1; it would be 1 less than the array index (sizeof(arr)-1)
+            return true;
+        else
+            return false;
+    }
 
     // check if queue is empty
-    void isEmpty() {}
+    bool isEmpty()
+    {
+        if (front == -1 && rear == -1)
+            return true;
+        else
+            return false;
+    }
 
     // total items in queue
     void count() {}
@@ -26,6 +51,8 @@ public:
 
 int main()
 {
+    Queue q;
+    cout << q.isEmpty();
 
     return 0;
 }
