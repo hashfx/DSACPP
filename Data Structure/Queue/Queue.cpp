dd1e2061209429e70c450b85a49ae9b1aff564db
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-09-06 15:51:23 +0000

    Queue Boiler

---
+++ Data Structure/Queue/Queue.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+class Queue
+{
+    private:
+    int front;
+    int rear;
+    int arr[MAX];
+public:
+    // elements are added from rear end
+    void enqueue() {}
+
+    // elements are removed from front end
+    void dequeue() {}
+
+    // check if queue is full
+    void isFull() {}
+
+    // check if queue is empty
+    void isEmpty() {}
+
+    // total items in queue
+    void count() {}
+};
+
+int main()
+{
+
+    return 0;
+}
