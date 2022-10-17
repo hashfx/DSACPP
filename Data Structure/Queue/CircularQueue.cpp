a102fb44558dfdafd5ea61800c6cb4f3389d20fa
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-10-17 11:15:53 +0000

    enq dq count display prog of circQ

--- Data Structure/Queue/CircularQueue.cpp
+++ Data Structure/Queue/CircularQueue.cpp
@@ -22,14 +22,16 @@ Solution in Circular Queue:
 class CircularQueue
 {
 private:
-    int front; // removed from front end
-    int rear;  // added from rear end
-    int arr[MAX];
+    int front;     // removed from front end
+    int rear;      // added from rear end
+    int arr[MAX];  // size of array
+    int itemCount; // keeps track of items in the queue
 
 public:
     // constructor
     CircularQueue()
     {
+        itemCount = 0; // initialise items to 0
         // initialize front and rear to -1
         front = -1;
         rear = -1;
@@ -55,10 +57,11 @@ public:
             arr[rear] = value; // set rear to value
         }
         else
-        {                      // when queue is neither full nor empty
-            rear++;            // increase rear to next position
-            arr[rear] = value; // set value to current rear index in queue
+        {                                                      // when queue is neither full nor empty
+            rear = ((rear + 1) % (sizeof(arr) / sizeof(int))); // increase rear to next position in circular manner
+            arr[rear] = value;                                 // set value to current rear index in queue
         }
+        itemCount++; // increase item count
     }
 
     // elements are removed from front end
@@ -78,13 +81,15 @@ public:
             // set pointers back to -1
             rear = -1;
             front = -1;
+            itemCount--; // decrease item count
             return v;
         }
         else
-        {                   // when we have multiple values in queue
-            v = arr[front]; // store value of last element in a variable
-            arr[front] = 0; // remove value at arr[front] and set to 0
-            front++;        // set front to next value
+        {                                                        // when we have multiple values in queue
+            v = arr[front];                                      // store value of last element in a variable
+            arr[front] = 0;                                      // remove value at arr[front] and set to 0
+            front = ((front + 1) % (sizeof(arr) / sizeof(int))); // set front to next value
+            itemCount--;                                         // decrease item count
             return v;
         }
     }
@@ -92,7 +97,7 @@ public:
     // check if queue is full
     bool isFull()
     {
-        if ((rear+1)%(sizeof(arr)/sizeof(int)) == front) // being pointer starting from -1; it would be 1 less than the array index (sizeof(arr)-1)
+        if ((rear + 1) % (sizeof(arr) / sizeof(int)) == front) // being pointer starting from -1; it would be 1 less than the array index (sizeof(arr)-1)
             return true;
         else
             return false;
@@ -110,7 +115,16 @@ public:
     // total items in queue
     int count()
     {
-        return (rear - front + 1);
+        return itemCount;
+    }
+
+    // display all elements of queue
+    void display()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cout << arr[i] << "  ";
+        }
     }
 };
 
@@ -118,16 +132,19 @@ int main()
 {
     CircularQueue q;
     q.enqueue(1);
-    q.enqueue(1);
-    q.enqueue(1);
-    q.enqueue(1);
-    cout << q.isFull();
-    
-    cout << q.count();
-    
-    q.dequeue();
-    q.dequeue();
-    // q.dequeue();
-    cout << q.count();
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+
+    cout << "\nisFull:" << q.isFull();
+    cout << "\nCount: " << q.count();
+
+    cout << q.dequeue();
+    cout << q.dequeue();
+
+    cout << "\nCount: " << q.count() << endl;
+
+    q.display();
     return 0;
 }
