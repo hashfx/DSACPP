9212e3f4b6164cbedd699b5f4ea743eb3dff4cf4
Harsh Soni <algebra.soni@gmail.com>
2023-12-12 12:17:05 +0000

    quick sort algorithm

---
+++ Algorithm/Sort/quick_sort.cpp
@@ -0,0 +1,24 @@
+/*
+Quick Sort Algorithm (Divide and Conquer)
+
+    Sorts an array by recursively dividing it into two sub-arrays,
+    one containing elements less than a chosen pivot element,
+    and the other containing elements greater than or equal to the pivot.
+    This process repeats until each sub-array contains only one element (sorted).
+
+Time Complexity: O(n log n) on average, O(n^2) in worst case.
+
+Algorithm:
+    > Select a pivot element from the array (e.g., middle element).
+    > Partition the array into two sub-arrays:
+    > Left: elements less than the pivot.
+    > Right: elements greater than or equal to the pivot.
+    > Recursively apply Quick Sort to both left and right sub-arrays.
+    > Merge the sorted left and right sub-arrays back into the original array.
+
+Output Parameters:
+    [In] size of the array[n]
+    [In] pointer to the array of elements[arr]
+    [Op] sorted array
+*/
+
