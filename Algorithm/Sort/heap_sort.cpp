197be5aae98fa3d5c92d6ffe572789fe28b76894
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2023-12-13 18:22:14 +0530

    heap sort using iterative method

---
+++ Algorithm/Sort/heap_sort.cpp
@@ -0,0 +1,10 @@
+/*
+Heap Sort:
+    comparison-based sorting algorithm, uses a binary heap data structure
+    builds a max heap, iteratively extracts maximum element
+    
+output parameters:
+    [In] size of array [n]
+    [In] elements of array [arr]
+    [Op] sorted array
+*/
