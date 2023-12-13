8dc4eebfaa8830798d098822db6fdc784e3fe18e
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2023-12-13 18:07:28 +0530

    heap sort program

--- Algorithm/Sort/heap_sort.cpp
+++ Algorithm/Sort/heap_sort.cpp
@@ -8,3 +8,63 @@ Output Parameters:
     [In] elements of array [arr]
     [Op] sorted array
 */
+
+#include <iostream>
+using namespace std;
+
+// function to heapify a subtree rooted at the given index
+void heapify(int arr[], int n, int i) {
+    int largest = i;    // assume the current node is the largest
+    int left_child = 2 * i + 1;  // left child index
+    int right_child = 2 * i + 2;  // right child index
+
+    // check if left child is larger than the current largest node
+    if (left_child < n && arr[left_child] > arr[largest])
+        largest = left_child;
+
+    // check if right child is larger than the current largest node
+    if (right_child < n && arr[right_child] > arr[largest])
+        largest = right_child;
+
+    // if the largest node is not the current node, swap them and recursively heapify
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+// function to perform heap sort on the given array
+void heap_sort(int arr[], int n) {
+    // Build a max heap
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // extract elements one by one from the heap
+    for (int i = n - 1; i > 0; i--) {
+        // swap the root with the last element
+        swap(arr[0], arr[i]);
+        // heapify the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;  // input size of the array
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];  // input array elements
+    }
+
+    // Perform heap sort
+    heap_sort(arr, n);
+
+    // display the sorted array
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
